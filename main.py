186517c1f381fbbf596bff6069d0866929c7e81f
coffee2boss <100610909+coffee2boss@users.noreply.github.com>
2022-04-04 17:06:09 +0100

    Create main.py
    starter file for main

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+from turtle import Screen, color
+from paddles import Paddle
+from ball import Ball
+from score import Scoreboard
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG GAME")
+screen.tracer(0)
+
+
+
+
+
+
+screen.exitonclick()
